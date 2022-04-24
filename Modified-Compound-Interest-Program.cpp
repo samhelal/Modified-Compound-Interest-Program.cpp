429fff9c963d8f55353ae7b8388842bd8db96bb8
samhelal <93845103+samhelal@users.noreply.github.com>
2022-04-24 05:28:59 +0200

    Add files via upload

---
+++ Modified-Compound-Interest-Program.cpp
@@ -0,0 +1,26 @@
+/*(Modified Compound-Interest Program) Modify the compound-interest application 
+of Fig. 5.6 to repeat its steps for interest rates of 5%, 6%, 7%, 8%, 9% and 10%.
+Use a for loop to vary the interest rate.(c++ how to program)*/
+#include <iostream>
+#include<cmath>
+#include <iomanip>
+using namespace std ;
+
+int main (){
+
+    double principal (1000) ;
+    double rate (0) ;
+    cout << setprecision(2) << fixed ;
+    cout << " Please Enter rate " ;
+    cin >> rate ;
+    cout << " Intial pricipal : " << principal << endl ;
+    cout << "    Interst rate :    " << rate << endl ;
+    cout << "\n year " << setw(20) << "Amount on deposite" << endl ; 
+
+    for(unsigned year(1); year <= 10 ; year++){
+
+        double amount = principal * pow(1+rate,year) ;
+        cout << setw(4) << year << setw(20)<< amount << endl ;
+    }
+
+}
